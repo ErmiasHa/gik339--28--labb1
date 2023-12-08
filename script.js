082493577300cff1d72d0bc0398726e850abd882
ErmiasHa <150346680+ErmiasHa@users.noreply.github.com>
2023-12-08 19:13:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Variabel 1: Checkboxen
     const checkboxElement = document.getElementById("divStyle");
 
-    // Variabel 2: Alla textfält
+   //Alla textfält
     const textfieldsCollection = document.getElementsByClassName("textfield");
     const textfieldsArray = Array.from(textfieldsCollection);
 
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Variabel 4: Div-elementet
     const customDivElement = document.getElementById("customDiv");
 
-    // Uppgift 5 - Skapa en fördefinierad funktion
+    
     function handleInput(e) {
         console.log('Target:', e.target);
 
@@ -25,22 +25,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Eventlyssnare till checkboxen
+   
     checkboxElement.addEventListener("change", function () {
-        // Hämta färgen från input-fältet med namn/id "color"
+ 
         const colorInput = document.getElementById("color");
         const backgroundColor = colorInput.value;
 
-    
-        customDivElement.style.backgroundColor = backgroundColor;
+      customDivElement.style.backgroundColor = backgroundColor;
     });
 
-    // Eventlyssnare till textfälten
+ 
     textfieldsArray.forEach(function (textField) {
         textField.addEventListener("input", handleInput);
     });
 
-    // Eventlyssnare till knappen
+
     submitButton.addEventListener("click", function () {
         // Ta bort div-elementet från DOM-trädet
         customDivElement.remove();
